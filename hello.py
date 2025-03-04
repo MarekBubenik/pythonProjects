bd56ceed0a42e6f4525b6e3866ee42a9463250ed
T-1000 <b_mar@tuta.io>
2025-03-04 16:24:05 +0100

    Hello, world!

---
+++ hello.py
@@ -0,0 +1,15 @@
+# Ask user for their name
+name = input("What's your name? ")
+
+# Say hello to user
+print("hello, " + name)
+print("hello,", name)
+
+print("Hello ,", end="")
+print(name)
+
+#print(*objects, sep='', end='\n', file=sys.stdout, flush=False)
+
+print('Hello, "friend"')
+print("Hello, \"friend\"")
+print(f"Hello, {name}")
