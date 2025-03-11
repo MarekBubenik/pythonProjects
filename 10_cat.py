a265ba0aba0376445216eebfb8df5d00a68aaf71
T-1000 <b_mar@tuta.io>
2025-03-11 16:17:49 +0100

    renamed files, loops, list

---
+++ 10_cat.py
@@ -0,0 +1,13 @@
+# print("Meow")
+# print("Meow")
+# print("Meow")
+
+# i = 3
+# while i != 0:
+#     print("meow")
+#     i = i - 1
+
+i = 0
+while i < 3:
+    print("meow")
+    i += 1
---
+++ 11_list.py
@@ -0,0 +1,22 @@
+# for i in [0, 1, 2]:
+#     print("meow")
+    
+# for _ in range(3):      # the underscore means I don't care about the variable, it's just there for the loop purposes
+#     print("meow")
+    
+for i in range(2):
+    print("meow")
+
+# print("meow\n" * 3, end="")
+
+while True:
+    n = int(input("What's n? "))
+    # if n < 0:
+    #     continue
+    # else:
+    #     break
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("meow")
